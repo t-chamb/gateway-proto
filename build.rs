48e25c0c65be50c43ea736ac0b9472bbb3a1d159
Sergey Matov <sergey.matov@githedgehog.com>
2025-04-17 16:58:19 +0400

    refactor: Split building of the crate
    If rust backend code has to be regenerated, use
    
      cargo build --feature regenerate
    
    Signed-off-by: Sergey Matov <sergey.matov@githedgehog.com>

--- build.rs
+++ build.rs
@@ -2,20 +2,26 @@
 // SPDX-License-Identifier: Apache-2.0
 
 fn main() {
-    let proto = "proto/dataplane.proto";
+    #[cfg(feature = "regenerate")] {
+        let proto = "proto/dataplane.proto";
 
-    let res = tonic_build::configure()
-        .type_attribute(".", "#[derive(::serde::Deserialize, ::serde::Serialize)]")
-        .build_server(true)
-        .build_client(true)
-        .out_dir("src/generated")
-        .compile_protos(&[proto], &["proto"]);
+        let res = tonic_build::configure()
+            .type_attribute(".", "#[derive(::serde::Deserialize, ::serde::Serialize)]")
+            .build_server(true)
+            .build_client(true)
+            .out_dir("src/generated")
+            .compile_protos(&[proto], &["proto"]);
 
-    match res {
-        Ok(_) => println!("Protobuf compiled successfully!"),
-        Err(e) => {
-            eprintln!("Failed to compile Protobuf: {}", e);
-            std::process::exit(1);
+        match res {
+            Ok(_) => println!("Protobuf compiled successfully!"),
+            Err(e) => {
+                eprintln!("Failed to compile Protobuf: {}", e);
+                std::process::exit(1);
+            }
         }
+    }
+    #[cfg(not(feature = "regenerate"))]
+    {
+        // Do nothing
     }
 }
